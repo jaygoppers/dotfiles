1d1e6411d25b8f31fbb9d8b9cb75e76f265142a7
Jeremie <jeremieforest3@gmail.com>
2021-01-21 22:19:23 -0500

    Multiple change to polybar, remove some rule from bspwm

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,12 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -rq music &
-polybar -rq tray &
-polybar -rq date &
-polybar -rq wsnumber &
-
+# Launch bars
+#polybar -rq wsnumber &
+#polybar -rq music &
+#polybar -rq tray &
+#polybar -rq essentials &
+polybar -rq top &
+polybar -rq bottom &
 echo "Polybar launched..."
 
 
