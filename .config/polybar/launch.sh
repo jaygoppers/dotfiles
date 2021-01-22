f59f1b81c1da1bd1da0f2400a458f3bae940f68d
Jeremie <jeremieforest3@gmail.com>
2021-01-21 23:02:05 -0500

    Change colorscheme, deleted some comment

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,10 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-#polybar -rq wsnumber &
-#polybar -rq music &
-#polybar -rq tray &
-#polybar -rq essentials &
 polybar -rq top &
 polybar -rq bottom &
 echo "Polybar launched..."
